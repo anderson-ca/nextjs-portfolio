32a40ee943784634f839a7dc6c2c8746ab4bc687
anderson <anderson.d.cardoso92@gmail.com>
2022-02-23 09:34:00 -0600

    add dog model in hero section

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,8 +5,11 @@ const Home = () => {
 
   return (
     <div className={styles.home}>
-      <h1>hey</h1>
-      {/* <ThreeCanvas /> */}
+      <div className={`${styles.text_wrapper}`}>
+        <h2>Hello my name is Anderson</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At ullam vitae cum, officia id?</p>
+      </div>
+      <ThreeCanvas />
     </div>
   );
 }
