e41b097b7867a88e86632f736223cc431acb5d7c
anderson <anderson.d.cardoso92@gmail.com>
2022-03-01 14:28:47 -0600

    finish basic about page

--- components/Divider.jsx
+++ components/Divider.jsx
@@ -3,12 +3,11 @@ import styles from '../styles/Divider.module.css';
 const Divider = () => {
     return (
         <div className={`${styles.divider_wrapper}`}>
-            {/* <svg className={`${styles.svg}`} data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" className={styles.shape_fill}></path>
-            </svg> */}
+
             <svg className={`${styles.svg}`} data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
                 <path d="M598.97 114.72L0 0 0 120 1200 120 1200 0 598.97 114.72z" className={styles.shape_fill}></path>
             </svg>
+            <p className={`${styles.p} ${styles.top}`} >Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, vitae at? Laboriosam iste esse hic. Eos tempore ratione atque amet sed, labore placeat ad possimus iure dolorem necessitatibus molestiae accusantium!</p>
         </div>
     )
 }
--- pages/about.jsx
+++ pages/about.jsx
@@ -5,19 +5,24 @@ import Divider from '../components/Divider';
 const about = () => {
   return (
     <div className={styles.about}>
-      <Divider />
       <div className={`${styles.text_wrapper}`}>
         <div className={`${styles.text_bg}`}>
-
+          <h2>Title here</h2>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam architecto deserunt odit nulla, aut nisi perspiciatis facilis et, explicabo porro placeat officiis iusto vitae, eveniet provident maiores fugiat accusantium numquam!</p>
         </div>
-        <h2>Hey there</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, vitae at? Laboriosam iste esse hic. Eos tempore ratione atque amet sed, labore placeat ad possimus iure dolorem necessitatibus molestiae accusantium!</p>
       </div>
-      <div className={`${styles.svg_wrapper}`}>
-        <Image src='/fuckfuck.svg' alt='my head' height={'400rem'} width={'400rem'} />
-        <div className={`${styles.svg_body_wrapper}`}>
-          <Image src='/fuckyou-body.svg' alt='my head' height={'600rem'} width={'600rem'} />
+      <div className={`${styles.top}`}>
+        <div className={`${styles.divider_wrapper}`}>
+          <svg className={`${styles.svg}`} data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+            <path d="M598.97 114.72L0 0 0 120 1200 120 1200 0 598.97 114.72z" className={styles.shape_fill}></path>
+          </svg>
         </div>
+      </div>
+      <div className={`${styles.svg_figure} ${styles.head}`}>
+        <Image src='/fuckfuck.svg' alt='fuck' height={'500rem'} width={'500rem'} />
+      </div>
+      <div className={`${styles.svg_figure} ${styles.body}`}>
+        <Image src='/fuckyou-body.svg' alt='fuck' height={'800rem'} width={'800rem'} />
       </div>
     </div>
   )
