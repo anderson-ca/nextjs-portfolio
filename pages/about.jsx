a80fe47a0e1586a991a48e20b782b3ffbb590e28
anderson <anderson.d.cardoso92@gmail.com>
2022-03-01 07:32:51 -0600

    working on styling about me page

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,8 +1,25 @@
 import styles from '../styles/About.module.css';
+import Image from 'next/image';
+import Divider from '../components/Divider';
 
 const about = () => {
   return (
-    <div className={styles.about}>about</div>
+    <div className={styles.about}>
+      <Divider />
+      <div className={`${styles.text_wrapper}`}>
+        <div className={`${styles.text_bg}`}>
+
+        </div>
+        <h2>Hey there</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, vitae at? Laboriosam iste esse hic. Eos tempore ratione atque amet sed, labore placeat ad possimus iure dolorem necessitatibus molestiae accusantium!</p>
+      </div>
+      <div className={`${styles.svg_wrapper}`}>
+        <Image src='/fuckfuck.svg' alt='my head' height={'400rem'} width={'400rem'} />
+        <div className={`${styles.svg_body_wrapper}`}>
+          <Image src='/fuckyou-body.svg' alt='my head' height={'600rem'} width={'600rem'} />
+        </div>
+      </div>
+    </div>
   )
 }
 
--- pages/index.jsx
+++ pages/index.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     <div className={styles.hero}>
       <div className={`${styles.text_wrapper}`}>
         <h2 className={`${styles.hero_title}`}>Hi, I'm Anderson</h2>
-        <p className={`${styles.hero_subtitle}`}>I'm a creative software developer who loves creating beautiful things on the web 🦴</p>
+        <p className={`${styles.hero_subtitle}`}>I'm a inventive software developer who loves creating delightful experiences on the web🦴</p>
       </div>
       <ThreeCanvas />
     </div>
