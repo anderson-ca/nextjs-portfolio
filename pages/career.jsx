601bb5d217277dfa03dd4d1d6631d9d9831d6f63
anderson <anderson.d.cardoso92@gmail.com>
2022-03-03 10:28:31 -0600

    add arrow indicating overing section for better usability

--- pages/career.jsx
+++ pages/career.jsx
@@ -8,11 +8,11 @@ const career = () => {
         <Image src='/careerhead.svg' alt='my face' height={'800rem'} width={'800px'} />
       </div>
       <ul className={`${styles.ul}`}>
-        <li className={`${styles.li} ${styles.a}`}>
+        <li className={`${styles.li} ${styles.accenture}`}>
           <div className={`${styles.text_wrapper}`}>
             <h2 className={`${styles.text_shadow_pop_br} `}>Accenture</h2>
             <p className={`${styles.text_shadow_pop_br} `}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur at perspiciatis laudantium optio eveniet quam quasi! Maiores tempore asperiores culpa cum, ullam eligendi aliquam! Ducimus quisquam pariatur saepe non sapiente.</p>
-            <h3 className={`${styles.text_shadow_pop_br} `}>Software Engineer</h3>
+            <h4 className={`${styles.text_shadow_pop_br} `}>Software Engineer</h4>
             <p className={`${styles.text_shadow_pop_br} `}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur at perspiciatis laudantium optio eveniet quam quasi! Maiores tempore asperiores culpa cum, ullam eligendi aliquam! Ducimus quisquam pariatur saepe non sapiente.</p>
           </div>
         </li>
@@ -20,19 +20,22 @@ const career = () => {
           <div className={`${styles.text_wrapper}`}>
             <h2 className={`${styles.text_shadow_pop_br} `}>Klaviyo</h2>
             <p className={`${styles.text_shadow_pop_br} `}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur at perspiciatis laudantium optio eveniet quam quasi! Maiores tempore asperiores culpa cum, ullam eligendi aliquam! Ducimus quisquam pariatur saepe non sapiente.</p>
-            <h3 className={`${styles.text_shadow_pop_br} `}>Sales Engineer</h3>
+            <h4 className={`${styles.text_shadow_pop_br} `}>Sales Engineer</h4>
             <p className={`${styles.text_shadow_pop_br} `}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur at perspiciatis laudantium optio eveniet quam quasi! Maiores tempore asperiores culpa cum, ullam eligendi aliquam! Ducimus quisquam pariatur saepe non sapiente.</p>
           </div>
         </li>
-        <li className={`${styles.li} ${styles.c}`}>
+        <li className={`${styles.li} ${styles.oracle}`}>
           <div className={`${styles.text_wrapper}`}>
             <h2 className={`${styles.text_shadow_pop_br} `}>Oracle</h2>
             <p className={`${styles.text_shadow_pop_br} `}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur at perspiciatis laudantium optio eveniet quam quasi! Maiores tempore asperiores culpa cum, ullam eligendi aliquam! Ducimus quisquam pariatur saepe non sapiente.</p>
-            <h3 className={`${styles.text_shadow_pop_br} `}>Cloud Engineer</h3>
+            <h4 className={`${styles.text_shadow_pop_br} `}>Cloud Engineer</h4>
             <p className={`${styles.text_shadow_pop_br} `}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur at perspiciatis laudantium optio eveniet quam quasi! Maiores tempore asperiores culpa cum, ullam eligendi aliquam! Ducimus quisquam pariatur saepe non sapiente.</p>
           </div>
         </li>
       </ul>
+      <div className={`${styles.arrow_wrapper}`}>
+        <Image src='/arrow.svg' alt='my face' height={'100rem'} width={'100px'} />
+      </div>
     </div>
   )
 }
