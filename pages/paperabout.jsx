814eb15250c8bbc5233fd2bf28ebbf0355b19a1d
anderson <anderson.d.cardoso92@gmail.com>
2022-03-10 10:54:17 -0600

    working on about paper buttons

--- pages/paperabout.jsx
+++ pages/paperabout.jsx
@@ -12,13 +12,14 @@ const PaperAbout = () => {
   const mycontext = useAppContext();
   const uglyRef = useRef({});
   const sharedBtnState = mycontext.uglyBtn;
-  let rick = uglyRef.current['rick'];
-  let linda = uglyRef.current['linda'];
-  let steve = uglyRef.current['steve'];
 
-  console.log('🌟 about page state - ', sharedBtnState);
 
   useEffect(() => {
+    console.log('🌟 about page state - ', ugly_text_wrapper);
+    let rick = uglyRef.current['rick'];
+    let linda = uglyRef.current['linda'];
+    let steve = uglyRef.current['steve'];
+    let ugly_text_wrapper = uglyRef.current['ugly-text'];
 
 
 
@@ -26,11 +27,13 @@ const PaperAbout = () => {
       console.log('😄 active button -', sharedBtnState.name);
       console.log('🛸 hover - ', sharedBtnState.hover);
       console.log('👆 click - ', sharedBtnState.click);
+      ugly_text_wrapper.className = `${styles.ugly_text_wrapper} ${styles.show}`
       // rick.className = `${styles.fuck}`;
       // linda.className = `${styles.fuck}`;
       // steve.className = `${styles.fuck}`;
     } else {
       console.log('🚫 no active button...');
+      ugly_text_wrapper.className = `${styles.ugly_text_wrapper} ${styles.hide}`
       rick.className = `${styles.hide}`;
       linda.className = `${styles.hide}`;
       steve.className = `${styles.hide}`;
@@ -42,17 +45,27 @@ const PaperAbout = () => {
         console.log('rick case...');
 
         rick.className = `${styles.text_wrapper} ${styles.show}`;
-        // rick.children[0].className = `${styles.fuck}`;
+        rick.children[0].className = `${styles.ugly_title}`;
         rick.children[1].className = `${styles.hide}`;
+
+        if (sharedBtnState.click) {
+          console.log('click...');
+          rick.children[1].className = `${styles.show}`;
+        }
 
         break;
 
       case 'steve':
         console.log('steve case...');
 
-        steve.className = `${styles.text_wrapper} ${styles.show}`;
-        // steve.children[0].className = `${styles.fuck}`;
-        // steve.children[1].className = `${styles.hide}`;
+        steve.className = `${styles.text_wrapper} ${styles.show} ${styles.btn_title}`;
+        steve.children[0].className = `${styles.ugly_title}`;
+        steve.children[1].className = `${styles.hide}`;
+
+        if (sharedBtnState.click) {
+          console.log('click...');
+          steve.children[1].className = `${styles.show}`;
+        }
 
         break;
 
@@ -60,8 +73,13 @@ const PaperAbout = () => {
         console.log('linda case...');
 
         linda.className = `${styles.text_wrapper} ${styles.show}`;
-        // linda.children[0].className = `${styles.fuck}`;
+        linda.children[0].className = `${styles.ugly_title}`;
         linda.children[1].className = `${styles.hide}`;
+
+        if (sharedBtnState.click) {
+          console.log('click...');
+          linda.children[1].className = `${styles.show}`;
+        }
 
         break;
     }
@@ -70,48 +88,38 @@ const PaperAbout = () => {
 
   return (
     <div className={`${styles.about}`}>
-      <div className={`${styles.section} ${styles.top}`}>
-        <h1 className={`${styles.title}`}>HOWDY!</h1>
-        <ul className={`${styles.ul}`}>
-          <li className={`${styles.li} ${styles.c}`}>
-            <div className={`${styles.btn_wrapper}`}>
-              <FaceBtn uglyBtnName={'linda'} />
-            </div>
-            <div ref={el => uglyRef.current['linda'] = el} className={`${styles.text_wrapper}`}>
-              <h3>Where am I from?</h3>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit molestiae iusto, odit voluptates necessitatibus dicta consequuntur, deserunt repellat quibusdam nemo natus? Neque hic sunt maiores rerum labore ipsam, aspernatur architecto.</p>
-            </div>
-          </li>
-          <li className={`${styles.li} ${styles.b}`}>
-            <div className={`${styles.btn_wrapper}`}>
-              <FaceBtn uglyBtnName={'steve'} />
-            </div>
-            <div ref={el => uglyRef.current['steve'] = el} className={`${styles.text_wrapper}`}>
-              <h3>Who am I?</h3>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit molestiae iusto, odit voluptates necessitatibus dicta consequuntur, deserunt repellat quibusdam nemo natus? Neque hic sunt maiores rerum labore ipsam, aspernatur architecto.</p>
-            </div>
-          </li>
-          <li className={`${styles.li} ${styles.a}`}>
-            <div className={`${styles.btn_wrapper}`}>
-              <FaceBtn uglyBtnName={'rick'} />
-            </div>
-            <div ref={el => uglyRef.current['rick'] = el} className={`${styles.text_wrapper}`}>
-              <h3>What do I do?</h3>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit molestiae iusto, odit voluptates necessitatibus dicta consequuntur, deserunt repellat quibusdam nemo natus? Neque hic sunt maiores rerum labore ipsam, aspernatur architecto.</p>
-            </div>
-          </li>
-        </ul>
-      </div>
-      <div className={`${styles.section} ${styles.bottom}`}>
-        <div className={`${styles.p}`}>
-          <q>
-            The creation of a single world comes from a huge number of fragments and chaos.
-          </q>
-          <h3 className={`${styles.h3}`}>&mdash; Hayao Miyazaki</h3>
-        </div>
-        <div className={`${styles.guy_wrapper}`}>
-          <Image src='/guy.svg' alt='blah' height={'250px'} width={'250px'} />
-        </div>
+      <div ref={el => uglyRef.current['ugly-text'] = el} className={`${styles.ugly_text_wrapper}`}></div>
+      <ul className={`${styles.ul} ${styles.top}`}>
+        <li className={`${styles.li} ${styles.c}`}>
+          <div className={`${styles.btn_wrapper}`}>
+            <FaceBtn uglyBtnName={'linda'} />
+          </div>
+          <div ref={el => uglyRef.current['linda'] = el} className={`${styles.text_wrapper}`}>
+            <h3>Where am I from?</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit molestiae iusto, odit voluptates necessitatibus dicta consequuntur, deserunt repellat quibusdam nemo natus? Neque hic sunt maiores rerum labore ipsam, aspernatur architecto.</p>
+          </div>
+        </li>
+        <li className={`${styles.li} ${styles.b}`}>
+          <div className={`${styles.btn_wrapper}`}>
+            <FaceBtn uglyBtnName={'steve'} />
+          </div>
+          <div ref={el => uglyRef.current['steve'] = el} className={`${styles.text_wrapper}`}>
+            <h3>Who am I?</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit molestiae iusto, odit voluptates necessitatibus dicta consequuntur, deserunt repellat quibusdam nemo natus? Neque hic sunt maiores rerum labore ipsam, aspernatur architecto.</p>
+          </div>
+        </li>
+        <li className={`${styles.li} ${styles.a}`}>
+          <div className={`${styles.btn_wrapper}`}>
+            <FaceBtn uglyBtnName={'rick'} />
+          </div>
+          <div ref={el => uglyRef.current['rick'] = el} className={`${styles.text_wrapper}`}>
+            <h3>What do I do?</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit molestiae iusto, odit voluptates necessitatibus dicta consequuntur, deserunt repellat quibusdam nemo natus? Neque hic sunt maiores rerum labore ipsam, aspernatur architecto.</p>
+          </div>
+        </li>
+      </ul>
+      <div className={`${styles.bottom}`}>
+        <Image src='/about-bg-final.svg' alt='something' height={`100%`} width={`100%`} />
       </div>
     </div>
   )
